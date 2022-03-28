93be760c3baf22fb3de53b866986de7d699da108
muhammad.ahmed <muhammad.ahmed@sixlogics.com>
2022-03-28 16:57:38 +0500

    SignUp Form Design Initial Version Completed, Reactive formControl . Custom Validation -> matchPassword check before request sends Then request sends using GET Method

---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+export class User {
+
+  constructor (
+    public username: any,
+    public email: any,
+    public phone: any,
+    public title: any,
+    protected password: any,
+  )
+  {}
+}
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { URLS } from '../utils/URLS';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,8 +10,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  titles: any;
+  UserSignUpValues: any;
 
+  constructor(private http: HttpClient, public urls: URLS) {
+    this.titles = [
+      'Owner', 'Employee'
+    ];
+  }
+  signUpForm = new FormGroup({
+    UserName : new FormControl('',Validators.required),
+    UserEmail : new FormControl('',Validators.required),
+    UserTitle : new FormControl('',Validators.required),
+    UserPhone : new FormControl('',Validators.compose([Validators.required, Validators.pattern('[+]{1}[0-9]{3}[0-9]{7}')])),
+    UserPassword : new FormControl('',Validators.compose([Validators.required, Validators.minLength(8)])),
+    UserReTypePassword : new FormControl('',Validators.compose([Validators.required, Validators.minLength(8)]))
+  });
+  // get UserName() {
+  //   return this.UserName.get('UserName');
+  // }
+
+  onSignUp(userData: any) {
+    this.UserSignUpValues = userData;
+    if(this.matchPasswords(userData) == true)
+    {
+      this.signUpRequest();
+    } else {
+      console.log('Password Not Matched! Please makeSure that the password and Re-type Password Matches.');
+    }
+  }
+
+  matchPasswords(userData) {
+    if(userData.UserPassword != '' && userData.UserReTypePassword != '') {
+      if(userData.UserPassword == userData.UserReTypePassword) {
+        return true;
+      }else {
+        return false;
+      }
+    }
+  }
+
+  BtnAnimate(btnType) {
+    var elm = document.getElementById('signUp');
+    console.log(elm);
+    if(btnType == 'signUp') {
+      elm.classList.add('signUpBtnAnimate');
+    } else if(btnType == 'logIn') {
+      elm.classList.add('logInBtnAnimate');
+    }
+  }
+  signUpRequest() {
+    let url = this.urls.WorkBaseUrl+'Authentication/signUpAction.php?name='+this.UserSignUpValues.UserName+'&title='+this.UserSignUpValues.UserTitle
+      +'&email='+this.UserSignUpValues.UserEmail+'&phone='+this.UserSignUpValues.UserPhone+'&password='+this.UserSignUpValues.UserPassword;
+    this.http.get(url).subscribe({
+      next: (response)=> console.log(response),
+      error: (error) => console.log(error),
+    });
+  }
   ngOnInit() {
   }
 
